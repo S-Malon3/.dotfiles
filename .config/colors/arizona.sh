361b31f0e0d9f5d17944917c27f41599985ea1aa
SelbyM <31752555+SelbyMalone@users.noreply.github.com>
2025-06-08 15:57:20 +1000

    Update arizona.sh

--- .config/colors/arizona.sh
+++ .config/colors/arizona.sh
@@ -1,8 +1,8 @@
 # ~/.config/colors/arizona.sh
-BASE=				"#263542"
-TEXT=				"#FAF4FE"
-ACCENT=			"#50BCC0"
+BASE=         "#263542"
+TEXT=				  "#FAF4FE"
+ACCENT=			  "#50BCC0"
 ACCENT_LIGHT=	"#BDD1D8"
 ACCENT_DARK=	"#2D6973"
 HIGHLIGHT=		"#CB91DC"
-CONTRAST=		"#C4917E"
+CONTRAST=		  "#C4917E"
